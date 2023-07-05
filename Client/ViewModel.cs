dcc5ed25a73b8e88a7528eefda497b77f21239a0
Максим Капітанюк <maxik20192006max@gmail.com>
2023-07-05 23:50:54 +0300

    Add project files.

---
+++ Client/ViewModel.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Input;
+
+namespace Client
+{
+    internal class ViewModel
+    {
+        private UdpClient client = new UdpClient();
+
+        private bool isListening = false;
+
+        public string Ip { get; set; } = "127.0.0.1";
+
+        public string Port { get; set; } = "3737";
+
+        public string Message { get; set; } = "Lorem Ipsum!";
+
+        private ObservableCollection<string> messages { get; set; } =
+            new ObservableCollection<string>();
+
+        public IEnumerable<string> Messages => messages;
+
+        private RelayCommand? joinCommand;
+
+        public ICommand JoinCommand => joinCommand ??=
+            new RelayCommand(o => Join());
+
+        private RelayCommand? sendMessageCommand;
+
+        public ICommand SendMessageCommand => sendMessageCommand ??=
+            new RelayCommand(o => SendMessage(Message));
+
+        private RelayCommand? leaveCommand;
+
+        public ICommand LeaveCommand => leaveCommand ??=
+            new RelayCommand(o => Leave());
+
+        private async void SendMessage(string text)
+        {
+            IPEndPoint serverIp = new IPEndPoint(IPAddress.Parse(Ip), int.Parse(Port));
+            byte[] data = Encoding.UTF8.GetBytes(text);
+
+            await client.SendAsync(data, serverIp);
+        }
+
+        private async void Listen()
+        {
+            while (isListening)
+            {
+                UdpReceiveResult res = await client.ReceiveAsync();
+                string message = Encoding.UTF8.GetString(res.Buffer);
+                messages.Add(message);
+            }
+        }
+
+        private void Join()
+        {
+            SendMessage("<join>");
+            isListening = true;
+            Listen();
+        }
+
+        private void Leave()
+        {
+            SendMessage("<leave>");
+            isListening = false;
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    internal class Program
+    {
+        const int port = 3737;
+        const string JOIN_CMD = "<join>";
+        const string LEAVE_CMD = "<leave>";
+
+        static void Main(string[] args)
+        {
+            UdpClient server = new(port);
+
+            HashSet<IPEndPoint> members = new();
+
+            while (true)
+            {
+                IPEndPoint clientIp = null;
+
+                byte[] data = server.Receive(ref clientIp);
+                string message = Encoding.UTF8.GetString(data);
+
+                Console.WriteLine($"[{DateTime.Now.ToShortTimeString()}] - {message} | from {clientIp}");
+
+                switch (message)
+                {
+                    case JOIN_CMD:
+                        members.Add(clientIp);
+                        break;
+                    case LEAVE_CMD:
+                        members.Remove(clientIp);
+                        break;
+                    default:
+                        foreach (IPEndPoint ip in members)
+                            server.Send(data, ip);
+                        break;
+                }
+            }
+
+        }
+    }
+}
